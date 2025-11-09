1e15b49b6ad3ff615e117d43556c5c5b7fd42373
William <williamqmckenzie@gmail.com>
2025-11-09 00:48:58 -0500

    New navbar

--- app/components/landing/navbar.tsx
+++ app/components/landing/navbar.tsx
@@ -1,17 +1,12 @@
-import { signIn } from "next-auth/react";
-
-export default function Navbar() {
+export default function Navbar(params : any) {
 
     return <div className="navbar bg-base-100">
-    <div className="navbar-start">
+      <div className="navbar-start"></div>
+      <div className="navbar-center"></div>
+      <div className="navbar-end">
+          <button className="btn btn-ghost" onClick={params.login}>
+              login
+            </button>
+        </div>
     </div>
-    <div className="navbar-center">
-    </div>
-    <div className="navbar-end">
-        <button className="btn btn-ghost" onClick={() => signIn('google', { callbackUrl: "/app" })}>
-            login
-        </button>
-      </div>
-    </div>
-  );
 }
